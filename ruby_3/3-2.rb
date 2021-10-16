bfc7d2d7dab9581ec734fdb7bdfee3fa2cd97369
hiraitomoyuki <kotemo0605@gmail.com>
2021-10-16 14:42:50 +0900

    3.3

---
+++ ruby_3/3-2.rb
@@ -0,0 +1,17 @@
+# テストコード雛形
+require 'minitest/autorun'
+
+class SampleTest < Minitest::Test
+  def test_sample
+    assert_equal 'RUBY', 'ruby'.upcase
+  end
+end
+
+# aとbが等しければパスする
+assert_equal b, a
+
+# aが真であればパスする
+assert a
+
+# aが偽であればパスする
+refute a
