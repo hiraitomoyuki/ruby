e31b97d657440169559731d1a335c3ff778efc0e
hiraitomoyuki <kotemo0605@gmail.com>
2021-11-06 22:38:01 +0900

    5.4.1

--- ruby_5/5_4.rb
+++ ruby_5/5_4.rb
@@ -6,4 +6,17 @@ currencies = { :japan => 'yen', :us => 'dollar', :india => 'rupee' }
 currencies[:us] # => 'dollar'
 
 # 新しいキーと値の組み合わせを追加する
-currencies
+currencies[:italy] = 'eurro'
+
+# シンボルがキーになる場合、 =>を使わずに"シンボル: 値"という記法でハッシュを作成できる。コロンの位置が左から右に変わる
+# =>ではなく、"シンボル: 値"の記法でハッシュを作成する
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+# 値を取り出すときは同じ
+currencies[:us] # => 'dollar'
+
+# キーも値もシンボルの場合は
+{ japan: :yen, us: :dollar, india: :rupee }
+# 同じことを違う書き方で
+{ :japan => :yen, us: :dollar, india: :rupee }
+# コロン同士で向き合うので不自然な印象を受けるかもしれないが、Rubyではこのようなハッシュの記述を使っていく
+
