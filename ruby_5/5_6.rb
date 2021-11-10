b2645ab978c860e1b18cf9951a938b1f5c8fdc2d
hiraitomoyuki <kotemo0605@gmail.com>
2021-11-10 18:28:57 +0900

    5.6.2

--- ruby_5/5_6.rb
+++ ruby_5/5_6.rb
@@ -12,3 +12,18 @@ currencies.values # => ["yen", "dollar", "rupee"]
 currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
 currencies.has_key?(:japan) # => true
 currencies.has_key?(:italy) # => false
+
+# **でハッシュを展開させる
+# **をハッシュの前につけるとハッシュリテラル内でほかのハッシュのキーと値を展開することができる
+h = {us: 'dollar', india: 'rupee' }
+# 変数hのキーと値を**で展開させる
+{ japan: 'yen', **h } # => {:japan=>"yen", :us=>"dollar", :india=>"ruppe"}
+
+# **をつけない場合は構文エラーになる
+{ japan: 'yen', h }
+# => SyntaxError: syntax error, unexpected '}', expecting =>
+#    { japan: 'yen', h }
+#                       ^
+# 上のコードは**のかわりにmergeメソッドを使っても同じ結果が得られる
+h = {us: 'dollar', india: 'rupee' }
+{ japan: 'yen' }.merge(h) # => {:japan=>"yen", :us=>"dollar", :india=>"ruppe"}
