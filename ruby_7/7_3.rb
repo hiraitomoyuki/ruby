588cbd4c2ce1d1eda6e794d9797e38f3fe1d8aed
hiraitomoyuki <kotemo0605@gmail.com>
2021-11-17 18:23:26 +0900

    7.3

---
+++ ruby_7/7_3.rb
@@ -0,0 +1,13 @@
+# クラスの定義
+# Rubyのクラスを定義する場合は
+class クラス名
+end
+# クラス名は必ず大文字で始める。小文字で始めると構文エラーになる。慣習として、UserやOrderItemのようにキャメルケースで書くのが一般的
+# Userクラスの定義
+class User
+end
+
+# OrderItemクラスの定義
+class OrderItem
+end
+
