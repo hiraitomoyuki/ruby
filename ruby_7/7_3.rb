0ed46ef2676844a5f82e89be3919d4322cbe6bef
hiraitomoyuki <kotemo0605@gmail.com>
2021-11-17 18:30:41 +0900

    7.3.1

--- ruby_7/7_3.rb
+++ ruby_7/7_3.rb
@@ -11,3 +11,27 @@ end
 class OrderItem
 end
 
+# オブジェクトの作成とinitializeメソッド
+# クラスからオブジェクトを作成する場合は以下のようにnewメソッドを使う
+User.new
+# この時に呼ばれるのがinitializeメソッド。インスタンスを初期化するために実行したい処理があれば、このinitializeメソッドでその処理を実装する(とくに必要がなければ定義しなくても大丈夫)
+class User
+  def initialize
+    puts 'Initialized.'
+  end
+end
+User.new
+# => Initialized.
+# initializeメソッドは特殊なメソッドで、外部から呼び出すことはできない(デフォルトでprivateメソッドになっている)
+user = User.new
+user.initialize
+# => NoMethodError: private method 'initialize' called for #<User:0x007fb18a321ca0>
+
+# initializeメソッドに引数をつけると、newメソッドを呼ぶ時にも引数が必要になる
+class User
+  def initialize(name, age)
+    puts "name: #{name}, age: #{age}"
+  end
+end
+User.new # => ArgumentError: wrong number of arguments (given 0, expected 2)
+User.new('Alice', 20) # => name: Alice, 20
