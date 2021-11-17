a09336e8395eaeb0768e207a14a1592cf4156643
hiraitomoyuki <kotemo0605@gmail.com>
2021-11-17 20:56:20 +0900

    7.4.2

--- ruby_book/test/gate_test.rb
+++ ruby_book/test/gate_test.rb
@@ -1,9 +1,13 @@
 require 'minitest/autorun'
 require './ruby_book/lib/gate'
+require './ruby_book/lib/ticket'
 
 class GateTest < Minitest::Test
   def test_gate
-    # とりあえずGateオブジェクトが作れることを確認する
-    assert Gate.new
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+    ticket = Ticket.new(150)
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
   end
 end
