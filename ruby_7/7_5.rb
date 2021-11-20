7d777c69bb3738e7a82e1eb31ac7876b8892cadc
hiraitomoyuki <kotemo0605@gmail.com>
2021-11-20 19:11:11 +0900

    7.5.2

--- ruby_7/7_5.rb
+++ ruby_7/7_5.rb
@@ -145,3 +145,37 @@ end
 # => "Hello!"
 #    "Hello!"
 #    "Hello!"
+
+# クラスメソッドをインスタンスメソッドで呼び出す
+# クラスメソッドをインスタンスメソッドの内部から呼び出す場合
+クラス名.メソッド
+
+class Product
+  attr_render :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  # 金額を整形するクラスメソッド
+  def self.format_price(price)
+    "#{price}円"
+  end
+
+  def to_s
+    # インスタンスメソッドからクラスメソッドを呼び出す
+    formatted_price = Product.format_price(price)
+    "name: #{name}, price: #{formatted_price}"
+  end
+end
+
+product = Product.new('A great movie', 1000)
+product.to_s # => "name: A great movie, price: 1000円"
+
+# "クラス名.メソッド"とかく代わりに、"self.class.メソッド"のように書く場合もある。
+# クラス名.メソッドの形式でクラスメソッドを呼び出す
+Product.format_price(price)
+
+# self.class.メソッドの形式でクラスメソッドを呼び出す
+self.class.format_price(price)
