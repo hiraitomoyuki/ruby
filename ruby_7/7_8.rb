79bab6b612b046016a0cc05ed0405e02dd08ce0b
hiraitomoyuki <kotemo0605@gmail.com>
2021-12-05 17:02:02 +0900

    7.8.2

--- ruby_7/7_8.rb
+++ ruby_7/7_8.rb
@@ -63,3 +63,88 @@ class Product
   freeze
   DEFAULT_PRICE = 1000 # => RuntimeError: can't modify frozen #<Class:Product>
 end
+
+# 定数はミュータブルなオブジェクトに注意する
+# 再代入をしなくてもミュータブルなオブジェクトであれば定数の値を変えることができる。ミュータブルなオブジェクトとは、例えば文字列やハッシュなど。
+# 定数の値を破壊的に変更してしまうコード例
+class Product
+  NAME = 'A product'
+  SOME_NAMES = ['Foo', 'Bar', 'Baz']
+  SOME_PRICES = { 'Foo' => 1000, 'Bar' => 2000, 'Baz' => 3000 }
+end
+
+# 文字列を破壊的に大文字に変更する
+Product::NAME.upcase!
+Product::NAME # => "A PRODUCT"
+
+# 配列に新しい要素を追加する
+Product::SOME_NAMES << 'Hoge'
+Product::SOME_NAMES # => ["Foo", "Bar", "Baz", "Hoge"]
+
+# ハッシュに新しいキーと値を追加する
+Product::SOME_PRICES['Hoge'] = 4000
+Product::SOME_PRICES # => {"Foo"=>1000, "Bar"=>2000, "Baz"=>3000, "Hoge"=>4000}
+
+# 定数の中身が変更されてしまった。上のコードは定数を直接変更しているので見た目にも「定数を変更している」ということが明らかだが、定数の値を変数に代入してしまうと定数を変更していることに気づきにくい
+class Product
+  SOME_NAMES = ['Foo', 'Bar', 'Baz']
+  def self.names_without_foo(names = SOME_NAMES)
+    # nameがデフォルト値だと、以下のコードは定数のSOME_NAMESを破壊的に変更していることになる
+    names.delete('Foo')
+    names
+  end
+end
+
+Product.names_without_foo # => ["Bar", "Baz"]
+
+# 定数の中身が変わってしまった！
+Product::SOME_NAMES       # => ["Bar", "Baz"]
+
+# こうした事故を防ぐためには、定数の値をfreezeする。こうすると定数に対して破壊的な変更ができなくなる
+class Product
+  # 配列を凍結する
+  SOME_NAMES = ['Foo', 'Bar', 'Baz'].freeze
+
+  def self.names_without_foo(names = SOME_NAMES)
+    # freezeしている配列に対しては破壊的な変更はできない
+    names.delete('Foo')
+    names
+  end
+end
+
+# エラーが発生するのでうっかり定数の値が変更される事故が防げる
+Product.names_without_foo # => RuntimeError: can't modify frozen Array
+
+# これでもまだ完璧ではない。配列やハッシュをfreezeすると配列やハッシュそのものへの変更は防止できますが、配列やハッシュの各要素はfreezeしない。よって、次のようなコードを書くと定数の内容はやはり変更されてしまう
+class Product
+  # 配列はfreezeされるが中身の文字列はfreezeされない
+  SOME_NAMES = ['Foo', 'Bar', 'Baz'].freeze
+end
+# 1番目の要素を破壊的に大文字に変更する
+Product::SOME_NAMES[0].upcase!
+# 1番目の要素の値が変わってしまった！
+Product::SOME_NAMES # => ["FOO", "Bar", "Baz"]
+
+# この事故も防ぎたいとなると、各要素の値を別途freezeする必要がある
+class Product
+  # 中身の文字列もfreezeする
+  SOME_NAMES = ['Foo'.freeze, 'Bar'.freeze, 'Baz'.freeze].freeze
+end
+# 今度は中身もfreezeしているので破壊的な変更はできない
+Product::SOME_NAMES[0].upcase! # => RuttimeError: can't modify frozen String
+
+# なお、次のようにmapメソッドを使うと、freezeを何度も書かずに済む。
+
+# mapメソッドで各要素をfreezeし、最後にmapメソッドの戻り値の配列をfreezeする
+SOME_NAMES = ['Foo', 'Bar', 'Baz'].map(&:freeze).freeze
+
+# 配列やハッシュの中身まで全てfreezeするのは少し大変かもしれない。プログラムの規模や要件(堅牢性がどこまで重視されるかなど)に応じて、freezeを適用するレベル(深さ)を検討する
+# 一方、イミュータブルなオブジェクトはfreezeする必要がないことも押さえておく。数値やシンボル、true/falseなどはイミュータブルなオブジェクトなので破壊的に変更することはできない
+class Product
+  # 数値やシンボル、true/falseはfreeze不要(してもかまわないが、意味がない)
+  SOME_VALUE = 0
+  SOME_TYPE = :foo
+  SOME_FLAG = true
+end
+
+# Rubyの定数は「絶対変更できない値」ではなく、むしろ定数であっても「変更しようと思えばいくらでも変更できる値」になっている。定数にしておいたから大丈夫だと思い込まず、「ついうっかり」の事故を防ぐためには幾らかの工夫が必要になることを覚えておく。
