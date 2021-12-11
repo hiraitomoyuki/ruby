d1b67195a2318985ba86775c6bc570ac090b3987
hiraitomoyuki <kotemo0605@gmail.com>
2021-12-11 17:56:14 +0900

    7.10.2

--- ruby_7/7_10.rb
+++ ruby_7/7_10.rb
@@ -16,3 +16,16 @@ user = User.new
 user.hello    # => "Hello!"
 user.greeting # => "Hello!"
 # エイリアスは元のメソッドに新しい機能を追加する場合にもよく使われる。
+
+# メソッドの削除
+# 頻繁に使う機能ではないが、Rubyではメソッドの定数をあとから削除することもできる。メソッドを削除する場合はundefキーワードを使う
+undef 削除するメソッドの名前
+
+# スーパークラス(Objectクラス)で定義されているfreezeメソッドを削除するコード例
+class User
+  # freezeメソッドの定義を削除する
+  undef freeze
+end
+user = User.new
+# freezeメソッドを呼び出すとエラーになる
+user.freeze # => NoMethodError: undefined method 'freeze' for #<User:0x007fea4d0c4d08>
